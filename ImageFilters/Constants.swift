bf4a25133731c9522a265d159d646182b2f4a545
bradleypj@icloud.com <bradleypj@icloud.com>
2015-04-07 10:02:43 -0700

    first commit

---
+++ ImageFilters/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  ImageFilters
+//
+//  Created by Bradley Johnson on 4/7/15.
+//  Copyright (c) 2015 BPJ. All rights reserved.
+//
+
+import Foundation
+
+let key = "osjdfoisdjfoisj"
